d36a98fee1ca4ba8485cd5fcbc4e2a762a333709
vcburnett <victorburnett@Victors-iMac-2.local>
2017-07-24 23:53:43 -0400

    Startup files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport" />
+<title>TITLE</title>
+<link rel="shortcut icon" href="assets/images/favicon.png"/>
+
+<link rel="stylesheet" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Titillium+Web:400,600" rel="stylesheet">
+
+</head>
+
+<body>
+
+</body>
+</html>
